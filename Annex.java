54385b5e0b162a648971c18b9e6ed848c4fafecd
AssassinLard <34082414+AssassinLard@users.noreply.github.com>
2019-04-23 11:39:54 +0800

    Add files via upload

---
+++ Annex.java
@@ -0,0 +1,15 @@
+package annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Annex
+{
+	String modelName();
+    String type();
+    String misc();
+}
